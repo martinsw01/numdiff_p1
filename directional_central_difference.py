6292ddc6c450889b2b872d6c8e49a15a3127561a
martinsw01 <martin.s.winther@gmail.com>
2023-02-16 14:16:49 +0100

    Implemented function computing directional central difference matrices

---
+++ directional_central_difference.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+def central_difference_matrix(d, M):
+    """
+
+    :param d: direction (x, y), tuple of integers
+    :param M: horizontal and vertical grid points
+    :return:
+    """
+
+    x, y = d
+
+    def coeff(i, j):
+        if i == j:
+            return - 2
+        elif i == j + y * (M - 1) - x:
+            return 1
+        elif i == j - y * (M - 1) + x:
+            return 1
+        else:
+            return 0
+
+    A = [[coeff(i, j)
+          for i in range((M - 1) ** 2)]
+         for j in range((M - 1) ** 2)]
+
+    return np.array(A)
+
+
+def test_along_x_axis():
+    M = 10
+
+    def tridiag(c, a, b, M):
+        e = np.ones(M)
+        return np.diag(a * e) + np.diag(b * e[:-1], 1) + np.diag(c * e[:-1], -1)
+
+    A = tridiag(1, -2, 1, (M - 1) ** 2)
+
+    assert (central_difference_matrix((1, 0), M) == A).all()
+
+
+if __name__ == '__main__':
+    test_along_x_axis()
