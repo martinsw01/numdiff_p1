285f3aa6b058db42e86ed893f7e67bcb65e4acff
martinsw01 <martin.s.winther@gmail.com>
2023-02-17 14:46:55 +0100

    Fixed the creation of central difference matrix, taking boundary conditions into account

--- directional_central_difference.py
+++ directional_central_difference.py
@@ -1,46 +1,19 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
-def central_difference_matrix(d, M):
-    """
-    Write a function that returns the matrix of the central difference
-    
-    :param d: direction (x, y), tuple of integers
-    :param M: horizontal and vertical grid points
-    :return:
-    """
-    dummy_test = 0
+def central_difference_matrix(M, k):
 
-    x, y = d
+    N = (M - 1) ** 2
+    A = -2 * np.eye(N)
 
-    def coeff(i, j):
-        if i == j:
-            return - 2
-        elif i == j + y * (M - 1) - x:
-            return 1
-        elif i == j - y * (M - 1) + x:
-            return 1
-        else:
-            return 0
+    off_diag = np.ones(N - k)
+    off_diag[M-2::M-1] = 0
+    A += np.diag(off_diag, k=k) + np.diag(off_diag, k=-k)
 
-    A = [[coeff(i, j)
-          for i in range((M - 1) ** 2)]
-         for j in range((M - 1) ** 2)]
-
-    return np.array(A)
-
-
-def test_along_x_axis():
-    M = 10
-
-    def tridiag(c, a, b, M):
-        e = np.ones(M)
-        return np.diag(a * e) + np.diag(b * e[:-1], 1) + np.diag(c * e[:-1], -1)
-
-    A = tridiag(1, -2, 1, (M - 1) ** 2)
-
-    assert (central_difference_matrix((1, 0), M) == A).all()
+    return A
 
 
 if __name__ == '__main__':
-    test_along_x_axis()
+    plt.pcolormesh(central_difference_matrix(5, 5))
+    plt.show()
