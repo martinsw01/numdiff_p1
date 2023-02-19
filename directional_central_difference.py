37008cf6183caf50ac4740aa266195abf412f21e
martinsw01 <martin.s.winther@gmail.com>
2023-02-19 20:28:27 +0100

    Take direction as argument

--- directional_central_difference.py
+++ directional_central_difference.py
@@ -2,14 +2,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def central_difference_matrix(M, k):
+def central_difference_matrix(M, d):
+    """
+    :param M: number of grid points along the axes
+    :param d: directon
+    :return: Central difference matrix approximating twice derivative along d
+    """
+
+    x, y = d
+
+    offset = (M-1) * y + x
 
     N = (M - 1) ** 2
     A = -2 * np.eye(N)
 
-    off_diag = np.ones(N - k)
+    off_diag = np.ones(N - offset)
     off_diag[M-2::M-1] = 0
-    A += np.diag(off_diag, k=k) + np.diag(off_diag, k=-k)
+    A += np.diag(off_diag, k=offset) + np.diag(off_diag, k=-offset)
 
     return A
 
