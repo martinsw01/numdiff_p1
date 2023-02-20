2713e5df1a92a2f7ccd054d84520caabc47e369d
martinsw01 <martin.s.winther@gmail.com>
2023-02-20 17:37:10 +0100

    Use test functions defined in test_functions.py

--- example.py
+++ example.py
@@ -1,41 +1,19 @@
 from anisitropic_he_solver import solve
+from test_functions import Test3, boundaries
 from viz import plot_solution, subplots_3d
-
-
-def T(x, y):
-    return x*(1-x)*y*(2-y)
-
-
-def g0(x):  # Bottom
-    return T(x, 0)
-
-
-def g1(y):  # Left
-    return T(0, y)
-
-
-def g2(y):  # Right
-    return T(1, y)
-
-
-def g3(x):  # Top
-    return T(x, 2)
-
-
-def rhs(r, a):
-    return lambda x, y: -(2*r**2*(x**2-x)+2*r*(x*y-x*(-y+2)-y*(-x+1)+(-x+1)*(-y+2))+2*(a+1)*(y**2-2*y))
 
 
 def main():
     a = 1
+    r = 2
     d1 = (1, 0)
     d2 = (1, 1)  # Adjusted after the grid, taking r into account (is actually (1, r))
     d = d1, d2
 
-    g = g0, g1, g2, g3
     M = 50
 
-    f = rhs(2, a)
+    T, f = Test3(r, a)
+    g = boundaries(T)
     X, Y, u = solve(a, d, g, f, M)
 
     _, (ax1, ax2) = subplots_3d(ncols=2)
