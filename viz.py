ee740fa32c8183deb82300373a5bb40fd1e305d4
martinsw01 <martin.s.winther@gmail.com>
2023-02-20 17:35:37 +0100

    Take ax as optional argument

--- viz.py
+++ viz.py
@@ -5,8 +5,11 @@ def subplots_3d(nrows=1, ncols=1, **fig_kw):
     return plt.subplots(nrows, ncols, subplot_kw={"projection": "3d"}, **fig_kw)
 
 
-def plot_solution(ax, X, Y, u, txt='Solution'):
+def plot_solution(X, Y, u, ax=None, txt='Solution'):
     # Plot the solution of the heat equation
+
+    if ax is None:
+        _, ax = subplots_3d()
 
     ax.plot_surface(X, Y, u, cmap=cm.coolwarm)
     ax.view_init(azim=30)  # Rotate the figure
