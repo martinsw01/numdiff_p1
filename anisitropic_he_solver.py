af5091d5c8e62818d8f19ab76e5e08cd0606a3f8
martinsw01 <martin.s.winther@gmail.com>
2023-02-22 14:16:26 +0100

    Fixed the adjustments in the corners of the grid. Was a source of major errors.

--- anisitropic_he_solver.py
+++ anisitropic_he_solver.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
 from directional_central_difference import central_difference_matrix
@@ -7,25 +6,25 @@ from directional_central_difference import central_difference_matrix
 def boundary_conditions(g, a, M, x, y):
     """
 
-    Add g to rhs
+    Adjust rhs wrt boundary
 
     :param g: g=(g0, g1, g2, g3) tuple of functions defined on the boundary
     :param x: array from 0 to 1 of size M+1
     :param y: array from 0 to 2 of size M+2
-    :return:
+    :return: adjustments to the rhs for elements close to the boundary
     """
     g0, g1, g2, g3 = g
     rhs = np.zeros((M-1, M-1))
 
-    rhs[0, 1:-1] = g0(x[1:M-2])  # Bottom
-    rhs[:, 0] = a*g1(y[1:M]) + g1(y[:M-1])  # Left
-    rhs[:, -1] = a*g2(y[1:M]) + g2(y[2:])  # Right
-    rhs[-1, 1:-1] = g3(x[3:-1])  # Top
+    # Adjust for derivative along d1
+    rhs[:, 0] += a*g1(y[1:-1])  # Left
+    rhs[:, -1] += a*g2(y[1:-1])  # Right
 
-    # rhs[:M-1] += g0(x[:M-1])
-    # rhs[::M-1] += g1(y[:M-1])
-    # rhs[M-2::M-1] += g2(y[2:])
-    # rhs[(M-1)*(M-2):] += g3(x[2:])
+    # Adjust for derivative along d2
+    rhs[0, :] += g0(x[:-2])  # Bottom
+    rhs[-1, :] += g3(x[2:])  # Top
+    rhs[1:, 0] += g1(y[1:-2])  # Left, skip first entry to avoid overlap with bottom
+    rhs[:-1, -1] += g2(y[2:-1])  # Right, skip last entry to avoid overlap with top
 
     return rhs.reshape((M-1)**2)
 
@@ -42,7 +41,8 @@ def solve(a, d, g, f, M):
 
     X, Y = np.meshgrid(x[1:-1], y[1:-1])
 
-    A = - a*central_difference_matrix(M, d[0])/h**2-central_difference_matrix(M, d[1])/h**2
+    d1, d2 = d
+    A = - a*central_difference_matrix(M, d1)/h**2-central_difference_matrix(M, d2)/h**2
     rhs = f(X, Y).reshape((M-1)**2)
     rhs += boundary_conditions(g, a, M, x, y)/h**2
 
