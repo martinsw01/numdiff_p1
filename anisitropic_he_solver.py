3a1582d8e943baec282f7913881ed397f5d2c238
martinsw01 <martin.s.winther@gmail.com>
2023-02-24 14:49:46 +0100

    Fatten bndry if r is irrational, and discretize grid into (M-1) points along x-axis, and (N-1) points along y-axis

--- anisitropic_he_solver.py
+++ anisitropic_he_solver.py
@@ -3,18 +3,18 @@ import numpy as np
 from directional_central_difference import central_difference_matrix
 
 
-def boundary_conditions(g, a, M, x, y):
+def boundary_conditions(g, a, M, N, x, y):
     """
 
-    Adjust rhs wrt boundary
+    Adjust rhs wrt boundary. Fattens bndry if Nk≠2.
 
     :param g: g=(g0, g1, g2, g3) tuple of functions defined on the boundary
     :param x: array from 0 to 1 of size M+1
-    :param y: array from 0 to 2 of size M+2
+    :param y: array from 0 to 2 of size N+1
     :return: adjustments to the rhs for elements close to the boundary
     """
     g0, g1, g2, g3 = g
-    rhs = np.zeros((M-1, M-1))
+    rhs = np.zeros((N-1, M-1))
 
     # Adjust for derivative along d1
     rhs[:, 0] += a*g1(y[1:-1])  # Left
@@ -26,26 +26,30 @@ def boundary_conditions(g, a, M, x, y):
     rhs[1:, 0] += g1(y[1:-2])  # Left, skip first entry to avoid overlap with bottom
     rhs[:-1, -1] += g2(y[2:-1])  # Right, skip last entry to avoid overlap with top
 
-    return rhs.reshape((M-1)**2)
+    return rhs.reshape((N-1)*(M-1))
 
 
-def solve(a, d, g, f, M):
+def solve(a, d, g, f, r, M, N=None):
     """
     :param d: tuple of vectors (d1, d2) specifying the direction of the heat flows
     :param g: g=(g0, g1, g2, g3) tuple of functions defined on the boundary
     :param f: rhs of the analytic equation
     :return: meshgrid (X, Y) and numeric solution u on the interior
     """
+
+    if N is None:
+        N = M
+
     x, h = np.linspace(0, 1, M+1, retstep=True)
-    y, k = np.linspace(0, 2, M+1, retstep=True)
+    y = np.linspace(0, r*h*N, N+1)  # Fattens the bndry if kN≠2
 
     X, Y = np.meshgrid(x[1:-1], y[1:-1])
 
     d1, d2 = d
-    A = - a*central_difference_matrix(M, d1)/h**2-central_difference_matrix(M, d2)/h**2
-    rhs = f(X, Y).reshape((M-1)**2)
-    rhs += boundary_conditions(g, a, M, x, y)/h**2
+    A = - a*central_difference_matrix(M, N, d1)/h**2-central_difference_matrix(M, N, d2)/h**2
+    rhs = f(X, Y).reshape((N-1)*(M-1))
+    rhs += boundary_conditions(g, a, M, N, x, y)/h**2
 
-    u = np.linalg.solve(A, rhs).reshape((M-1, M-1))
+    u = np.linalg.solve(A, rhs).reshape((N-1, M-1))
 
     return X, Y, u
--- convergence.py
+++ convergence.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from anisitropic_he_solver import solve
-from test_functions import boundaries, Test3
+from test_functions import *
 from viz import loglogplot_error
 
 
@@ -20,12 +20,35 @@ def main():
     e = np.empty_like(h)
 
     for i in range(len(M)):
-        X, Y, u = solve(a, d, g, f, M[i])
-        h[i] = 1/M[i]
+        X, Y, u = solve(a, d, g, f, r, M[i])
+        e[i] = np.max(np.abs(u-T(X, Y)))
+
+    loglogplot_error(h, e).show()
+
+
+def main_fatten_bdry(test_functions):
+    a = 1
+    r = np.sqrt(2)
+    d1 = (1, 0)
+    d2 = (1, 1)
+    d = d1, d2
+
+    T, f = test_functions(r, a)
+    g = boundaries(T)
+
+    M = np.logspace(1, 1.5, 20, dtype=int)
+    N = np.array(2*M/r, dtype=int) + 1
+    h = 1/M
+    e = np.empty_like(h)
+
+    for i in range(len(M)):
+        X, Y, u = solve(a, d, g, f, r, M[i], N[i])
         e[i] = np.max(np.abs(u-T(X, Y)))
 
     loglogplot_error(h, e).show()
 
 
 if __name__ == '__main__':
-    main()
+    main_fatten_bdry(Test2)
+    main_fatten_bdry(Test3)
+
