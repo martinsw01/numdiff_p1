e54fa6ebb982c6c8b99ea68036abb8546bfb6200
marcerichm <marcerichm@gmail.com>
2023-02-25 11:08:16 +0100

    Nå funker get_projection_point... for det meste.

--- utilities.py
+++ utilities.py
@@ -6,16 +6,23 @@ from sympy import Symbol
 this file holdes general utility-functions used in the project in genereal
 '''
 
+def isreal(num):
+    '''
+    :param num: number which we want to check is real or not
+    '''
+    if num.imag != 0:
+        result = False
+    else:
+        result = True
+
 def get_projection_point(p: tuple):
     '''
     :param p: A tuple reåresenting the point from which we want to project onto 1-x^2
     '''
     r = Symbol('r')
-    sol = solve(p[0] + r(1 - 2*p[1]) - 2*r**3, r)
-    if isinstance(sol[0], complex) or sol[0]<0: # picks the solution that is non-negative and non-complex
-        r = sol[1]
-    else: 
-        r = sol[0]
-    return (r, 1 - r**2) # returns the projection-point in 1-x^2
+    sol = solve(p[0] + r*(1 - 2*p[1]) - 2*r**3, r)
+    r = sol[0]
+    return (r, 1-r**2)
 
+print(get_projection_point((0.6, 1)))
 
